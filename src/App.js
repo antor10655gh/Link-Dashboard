34ea439e81bc2939c8004952375312dea891f42c
antor10655gh <freelancerantor@gmail.com>
2023-06-11 17:00:35 +0600

    add authentication

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Tables from "./pages/Tables";
 import Billing from "./pages/Billing";
 import Rtl from "./pages/Rtl";
 import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Main from "./components/layout/Main";
 import "antd/dist/antd.css";
@@ -12,20 +11,27 @@ import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
 
 function App() {
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <div className="App">
-      <Switch>
-        <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
-        <Main>
-          <Route exact path="/dashboard" component={Home} />
-          <Route exact path="/tables" component={Tables} />
-          <Route exact path="/billing" component={Billing} />
-          <Route exact path="/rtl" component={Rtl} />
-          <Route exact path="/profile" component={Profile} />
-          <Redirect from="*" to="/dashboard" />
-        </Main>
-      </Switch>
+      {
+        user ?
+        <Switch>
+          <Main>
+            <Route exact path="/dashboard" component={Home} />
+            <Route exact path="/tables" component={Tables} />
+            <Route exact path="/billing" component={Billing} />
+            <Route exact path="/rtl" component={Rtl} />
+            <Route exact path="/profile" component={Profile} />
+            <Redirect from="*" to="/dashboard" />
+          </Main>
+        </Switch> :
+        <Switch>
+          <Route path="/sign-in" exact component={SignIn} />
+          <Redirect from="*" to="/sign-in" />
+        </Switch>
+      }
+      
     </div>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,4 @@
-/*!
-  =========================================================
-  * Muse Ant Design Dashboard - v1.0.0
-  =========================================================
-  * Product Page: https://www.creative-tim.com/product/admin-antd
-  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-  * Licensed under MIT (https://github.com/creativetimofficial/admin-antd/blob/main/LICENSE.md)
-  * Coded by Creative Tim
-  =========================================================
-  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 import { useState } from "react";
-
 import {
   Row,
   Col,
